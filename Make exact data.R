8f43282b7194ddb46a2937cf60106b7a8b7700fd
jgdells <jgardell@u.rochester.edu>
2015-05-18 16:47:02 -0500

    This is a make exact data function

---
+++ Make exact data.R
@@ -0,0 +1,11 @@
+#Make exact data command
+
+z.score<-function(x){(x-mean(x))/sd(x)}
+
+rescale.numbers<- function(x,mean,sd){
+  z <- z.score(x)
+  return(z*sd + mean)}
+
+make.exact.data <- function(n, mean, sd){
+  x <- rnorm(n)
+  return(rescale.numbers(x,mean,sd))}
